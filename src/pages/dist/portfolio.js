1d4241f037afa65321925efa3fe1f762e36f24f3
minkukjo <strike0115@naver.com>
2021-06-19 03:41:38 +0900

    AOS 적용

---
+++ src/pages/dist/portfolio.js
@@ -0,0 +1,41 @@
+"use strict";
+exports.__esModule = true;
+var react_1 = require("react");
+var Layout_1 = require("@theme/Layout");
+var aos_1 = require("aos");
+require("aos/dist/aos.css");
+var handleScrollDown = function () {
+    window.scroll({
+        top: 1000,
+        behavior: 'smooth'
+    });
+};
+var Portfolio = function () {
+    react_1.useEffect(function () {
+        aos_1["default"].init({});
+    }, []);
+    return (react_1["default"].createElement(Layout_1["default"], { title: "Hello" },
+        react_1["default"].createElement("h1", { className: "hero__logo", "data-aos": "zoom-in" }, "Developer"),
+        react_1["default"].createElement("h2", { className: "hero__text", "data-aos": "fade-up", "data-aos-easing": "ease", "data-aos-delay": "400" },
+            react_1["default"].createElement("span", null, "Minkuk Jo")),
+        react_1["default"].createElement("div", { className: "hero__scroll", "data-aos": "fade-up", "data-aos-easing": "ease", "data-aos-delay": "800", onClick: handleScrollDown },
+            "Scroll down",
+            react_1["default"].createElement("i", { className: "chevron bottom" })),
+        react_1["default"].createElement("div", { style: {
+                paddingTop: '1000px',
+                display: 'block',
+                height: '100%',
+                fontSize: '20px',
+                margin: '50px'
+            } },
+            react_1["default"].createElement("div", { className: "item item--left", "data-aos": "fade-up" }),
+            react_1["default"].createElement("div", { className: "item item--right", "data-aos": "fade-left" }),
+            react_1["default"].createElement("div", { className: "item item--left", "data-aos": "fade-right" }),
+            react_1["default"].createElement("div", { className: "item item--right", "data-aos": "zoom-in" }),
+            react_1["default"].createElement("div", { className: "item item--left", "data-aos": "zoom-in-up" }),
+            react_1["default"].createElement("div", { className: "item item--right", "data-aos": "zoom-out" }),
+            react_1["default"].createElement("div", { className: "item item--left", "data-aos": "zoom-out-up" }),
+            react_1["default"].createElement("div", { className: "item item--right", "data-aos": "flip-left" }),
+            react_1["default"].createElement("div", { className: "item item--left", "data-aos": "flip-right" }))));
+};
+exports["default"] = Portfolio;
--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -1,17 +1,56 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Layout from '@theme/Layout'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
-function Portfolio() {
+const handleScrollDown = () => {
+  window.scroll({
+    top: 1000,
+    behavior: 'smooth'
+  })
+}
+
+const Portfolio = () => {
+  useEffect(() => {
+    AOS.init({})
+  }, [])
+
   return (
     <Layout title="Hello">
+      <h1 className="hero__logo" data-aos="zoom-in">
+        Developer
+      </h1>
+      <h2 className="hero__text" data-aos="fade-up" data-aos-easing="ease" data-aos-delay="400">
+        <span>Minkuk Jo</span>
+      </h2>
+      <div
+        className="hero__scroll"
+        data-aos="fade-up"
+        data-aos-easing="ease"
+        data-aos-delay="800"
+        onClick={handleScrollDown}
+      >
+        Scroll down
+        <i className="chevron bottom"></i>
+      </div>
       <div
         style={{
-          display: 'flex',
-          height: '50vh',
-          fontSize: '20px'
+          paddingTop: '1000px',
+          display: 'block',
+          height: '100%',
+          fontSize: '20px',
+          margin: '50px'
         }}
       >
-        두둥등장
+        <div className="item item--left" data-aos="fade-up"></div>
+        <div className="item item--right" data-aos="fade-left"></div>
+        <div className="item item--left" data-aos="fade-right"></div>
+        <div className="item item--right" data-aos="zoom-in"></div>
+        <div className="item item--left" data-aos="zoom-in-up"></div>
+        <div className="item item--right" data-aos="zoom-out"></div>
+        <div className="item item--left" data-aos="zoom-out-up"></div>
+        <div className="item item--right" data-aos="flip-left"></div>
+        <div className="item item--left" data-aos="flip-right"></div>
       </div>
     </Layout>
   )
